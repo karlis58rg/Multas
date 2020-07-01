e10417cb7badb08de7ce8d3021a2423ac2245121
Karlita <karlis58rg@gmail.com>
2020-07-01 06:59:54 -0500

    impresión_conceptos_libreria

--- app/src/main/java/mx/ssg/multas/NetPay.java
+++ app/src/main/java/mx/ssg/multas/NetPay.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,16 +14,14 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 
+import mx.com.netpay.sdk.IPage;
 import mx.com.netpay.sdk.SmartApi;
 import mx.com.netpay.sdk.SmartApiFactory;
-import mx.com.netpay.sdk.exceptions.SmartApiException;
 import mx.com.netpay.sdk.models.BaseResponse;
 import mx.com.netpay.sdk.models.Constants;
+import mx.com.netpay.sdk.models.PrintRequest;
 import mx.com.netpay.sdk.models.ReprintRequest;
 import mx.com.netpay.sdk.models.SaleRequest;
-import mx.com.netpay.sdk.models.SaleResponse;
-
-
 
 public class NetPay extends AppCompatActivity {
     EditText txtMonto,txtFolio,txtOrderId;
@@ -87,19 +86,60 @@ public class NetPay extends AppCompatActivity {
                 }
             }
         });
+
+        btnImpresion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Crear una página
+                IPage page = smartApi.createPage();
+
+                //Crear unidad que contiene texto y otros formatos
+                IPage.ILine.IUnit unit1 = page.createUnit();
+                unit1.setText("CONCEPTOS");
+                unit1.setGravity(Gravity.START);
+
+                //Se pueden agregar 2 o más unidades a una línea y se dividirá en columnas
+                IPage.ILine.IUnit unit2 = page.createUnit();
+                unit2.setText("MANEJAR EN ESTADO DE EBRIEDAD\n" + "DAR VUELTA EN LUGAR NO PERMITIDO");
+                unit2.setGravity(Gravity.END);
+
+                //Se crea una línea y se agregan sus unidades.
+                page.addLine().
+                        addUnit(unit1).
+                        addUnit(unit2);
+
+                //Se crea una nueva unidad
+                IPage.ILine.IUnit unit3 = page.createUnit();
+                unit3.setText("$3000.00");
+                unit3.setGravity(Gravity.CENTER);
+
+                //Se crea una nueva línea y se agrega la unidad pasada
+                page.addLine().addUnit(unit3);
+
+                //Se crea un request del tipo PrintRequest con el package name del app y la página creada
+                PrintRequest printRequest = new PrintRequest(appId, page);
+
+                smartApi.doTrans(printRequest);
+            }
+        });
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         Gson gson = new Gson();
         if(data != null){
-            if(requestCode == Constants.INSTANCE.getSALE_REQUEST()){
+            if(requestCode == Constants.SALE_REQUEST){
                 response = smartApi.onResult(requestCode,resultCode,data);
                 lblRespuesta.setText(gson.toJson(response));
-            }else if(requestCode == Constants.INSTANCE.getCANCEL_REQUEST()){
+            }else if(requestCode == Constants.CANCEL_REQUEST){
                 response = smartApi.onResult(requestCode,resultCode,data);
                 lblRespuesta.setText(gson.toJson(response));
-            }else if(requestCode == Constants.INSTANCE.getREPRINT_REQUEST()){
+            }else if(requestCode == Constants.REPRINT_REQUEST){
+                response = smartApi.onResult(requestCode,resultCode,data);
+                lblRespuesta.setText(gson.toJson(response));
+            }else if(requestCode == Constants.PRINT_REQUEST){
                 response = smartApi.onResult(requestCode,resultCode,data);
                 lblRespuesta.setText(gson.toJson(response));
             }else {
