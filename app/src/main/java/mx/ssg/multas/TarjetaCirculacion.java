e44ad8623d514a19cf46f2092ad8bebfa75afd5d
Karlita <karlis58rg@gmail.com>
2020-08-18 21:40:58 -0500

    getPlaca_baja

--- app/src/main/java/mx/ssg/multas/TarjetaCirculacion.java
+++ app/src/main/java/mx/ssg/multas/TarjetaCirculacion.java
@@ -581,6 +581,107 @@ public class TarjetaCirculacion extends AppCompatActivity {
         });
     }
 
+    /******************GET A BAJA CALIFORNIA***********************************/
+    public void getPlacaBJ() {
+        noPlacaTC = txtNoPlacaTC.getText().toString();
+        final OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url("http://187.174.102.142/AppTransito/api/TarjetaCirculacion?noPlacaToken="+noPlacaTC)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Looper.prepare();
+                Toast.makeText(getApplicationContext(),"ERROR AL OBTENER LA INFORMACIÓN, POR FAVOR VERIFIQUE SU CONEXIÓN A INTERNET",Toast.LENGTH_SHORT).show();
+                Looper.loop();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+                    TarjetaCirculacion.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                respuestaJson = "null";
+                                if(myResponse.equals(respuestaJson)){
+                                    Toast.makeText(getApplicationContext(),"NO SE CUENTA CON INFORMACIÓN",Toast.LENGTH_SHORT).show();
+                                }else{
+                                    JSONObject jObj = null;
+                                    jObj = new JSONObject(""+myResponse+"");
+                                   /* noTarjetaTC = jObj.getString("NoTarjeta");
+                                    noSerieTC = jObj.getString("NoSerie");
+                                    nombreTC = jObj.getString("NombreP");
+                                    aPaternoTC = jObj.getString("ApellidoPP");
+                                    aMaternoTC = jObj.getString("ApellidoMP");
+                                    nombreR = jObj.getString("NombreU");
+                                    aPaternoR = jObj.getString("ApellidoPU");
+                                    aMaternoR = jObj.getString("ApellidoMU");
+                                    noMotorTC = jObj.getString("NoMotor");
+                                    marca = jObj.getString("Marca");
+                                    subMarca = jObj.getString("SubMarca");
+                                    modelo = jObj.getString("Modelo");
+                                    color = jObj.getString("Color");
+                                    municipio = jObj.getString("Municipio");
+                                    localidad = jObj.getString("Localidad");
+                                    resOrigen = jObj.getString("Origen");
+                                    resServicio = jObj.getString("TipoServicio");
+                                    resObservaciones = jObj.getString("Observaciones");
+                                    txtNoTarjetaTC.setText(noTarjetaTC);
+                                    txtNoSerieTC.setText(noSerieTC);
+                                    txtNombreTC.setText(nombreTC);
+                                    txtApaternoTC.setText(aPaternoTC);
+                                    txtAmaternoTC.setText(aMaternoTC);
+                                    txtNombreR.setText(nombreR);
+                                    txtApaternoR.setText(aPaternoR);
+                                    txtAmaternoR.setText(aMaternoR);
+                                    txtNoMotorTC.setText(noMotorTC);
+                                    txtMarca.setText(marca);
+                                    txtSubmarca.setText(subMarca);
+                                    txtModelo.setText(modelo);
+                                    txtColor.setText(color);
+                                    txtMunicipio.setText(municipio);
+                                    txtLocalidad.setText(localidad);
+                                    origen = "Nacional";
+                                    servicio = "Privado";
+                                    observaciones = "null";
+                                    if(resOrigen.equals(origen)){
+                                        rNacional = (RadioButton)radioNacionalidadTC.getChildAt(0);
+                                        rNacional.setChecked(true);
+                                    }else {
+                                        rExtranjero = (RadioButton)radioNacionalidadTC.getChildAt(1);
+                                        rExtranjero.setChecked(true);
+                                    }*/
+                                    /*if(resServicio.equals(servicio)){
+                                        spinTipoServicio.setSelection(((ArrayAdapter<String>)spinTipoServicio.getAdapter()).getPosition("Privado"));
+                                    }else{
+                                        spinTipoServicio.setSelection(((ArrayAdapter<String>)spinTipoServicio.getAdapter()).getPosition("Publico"));
+                                    }*/
+                                    /*if(resObservaciones.equals(observaciones)){
+                                        txtObservaciones.setText("SIN OBSERVACIONES");
+                                    }else{
+                                        resObservaciones = jObj.getString("Observaciones");
+                                        txtObservaciones.setText(resObservaciones);
+                                    }*/
+                                    Log.i("HERE", ""+jObj);
+                                }
+
+                            }catch(JSONException e){
+                                e.printStackTrace();
+                            }
+
+                        }
+                    });
+                }
+            }
+
+        });
+    }
+
+
+
     //***************** INSERTA A LA BD MEDIANTE EL WS **************************//
     private void insertRegistroTarjeta() {
 
